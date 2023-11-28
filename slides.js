b7279554cf4db09b52934efae0cc86bdeb5f73f6
Ben Carson <ben03carson@gmail.com>
2023-11-28 23:14:50 +0300

    Onboarding Code

---
+++ slides.js
@@ -0,0 +1,27 @@
+export default [
+    {
+    id: '1',
+    title: 'Welcome to UniQuest!',
+    description: 'Embark on a journey with UniQuest, your gateway to exploring universities and courses worldwide.',
+    image: require('./assets/images/educator.png')
+    },
+    {
+    id: '2',
+    title: 'Discover Diverse Educational Hubs',
+    description: ' Find institutions tailored to your preferences, from renowned names to hidden gems.',
+    image: require('./assets/images/educator1.png')
+    },
+    {
+    id: '3',
+    title: 'Uncover Specialized Programs',
+    description: 'Unleash the spectrum of courses! Explore specialized programs, degrees, and certifications across disciplines.',
+    image: require('./assets/images/educator2.png')
+    },
+    {
+    id: '4',
+    title: 'Detailed Information at Your Fingertips',
+    description: 'Dig deeper into universities and courses. Access comprehensive details from fee structures to entry requirements.',
+    image: require('./assets/images/educator3.png')
+    },
+
+]
